89f053a1cd410102e6719565c3c90ce196e39243
xbtry <xbaturayx@outlook.com>
2024-05-13 21:59:33 +0300

    adding knobview

---
+++ SwiftUIViews/KnobView.swift
@@ -0,0 +1,18 @@
+//
+//  KnobView.swift
+//  SwiftUIViews
+//
+//  Created by baturay on 13.05.2024.
+//
+
+import SwiftUI
+
+struct KnobView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    KnobView()
+}
