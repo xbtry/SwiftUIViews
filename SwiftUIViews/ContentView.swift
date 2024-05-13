0d22d2c9068c6674236a05c0887eb5e8449edda2
xbtry <xbaturayx@outlook.com>
2024-05-13 21:57:00 +0300

    Initial Commit

---
+++ SwiftUIViews/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIViews
+//
+//  Created by baturay on 13.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIViews/SwiftUIViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIViewsApp.swift
+//  SwiftUIViews
+//
+//  Created by baturay on 13.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
